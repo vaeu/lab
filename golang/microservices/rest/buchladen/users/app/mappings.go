85155e48a4c2d553a3f616271e61522ffcc84234
vaeu <oneum@pm.me>
2022-11-02 06:30:24 +0000

    go: buchladen: get ‘/users’ endpoint to work

--- golang/microservices/rest/buchladen/users/app/mappings.go
+++ golang/microservices/rest/buchladen/users/app/mappings.go
@@ -4,4 +4,7 @@ import "github.com/vaeu/lab/golang/microservices/rest/buchladen/users/controller
 
 func mapURLs() {
 	router.GET("/ping", controller.Ping)
+	router.POST("/users", controller.CreateUser)
+	router.GET("/users/:user_id", controller.GetUser)
+	router.GET("/users/search", controller.SearchUser)
 }
--- golang/microservices/rest/buchladen/users/controller/users.go
+++ golang/microservices/rest/buchladen/users/controller/users.go
@@ -1,1 +1,19 @@
 package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "not implemented yet")
+}
+
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "not implemented yet")
+}
+
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "not implemented yet")
+}
