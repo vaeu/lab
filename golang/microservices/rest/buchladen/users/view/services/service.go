a3147dc0c1f14b6d4f2bf93dd2bdbfd7b6b5d664
vaeu <oneum@pm.me>
2022-11-07 07:53:01 +0000

    go: buchladen: validate users

--- golang/microservices/rest/buchladen/users/view/services/service.go
+++ golang/microservices/rest/buchladen/users/view/services/service.go
@@ -1,7 +1,13 @@
 package services
 
-import "github.com/vaeu/lab/golang/microservices/rest/buchladen/users/model/users"
+import (
+	"github.com/vaeu/lab/golang/microservices/rest/buchladen/users/model/users"
+	"github.com/vaeu/lab/golang/microservices/rest/buchladen/users/utils/errors"
+)
 
-func CreateUser(u users.User) (*users.User, error) {
+func CreateUser(u users.User) (*users.User, *errors.RESTErr) {
+	if err := users.Validate(&u); err != nil {
+		return nil, err
+	}
 	return &u, nil
 }
