b552b8d2f308e74f8bd86149e07731ff04534ab6
vaeu <oneum@pm.me>
2022-11-30 15:43:27 +0000

    go: buchladen: handle (in)valid queries by UID

--- golang/microservices/rest/buchladen/users/model/users/user_dao.go
+++ golang/microservices/rest/buchladen/users/model/users/user_dao.go
@@ -11,27 +11,29 @@ import (
 
 const (
 	indexUniqueEmail = "email_UNIQUE"
+	errNoRows        = "no rows in result set"
+	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
-var usersDB = make(map[uint64]*User)
-
 func (u *User) Get() *errors.RESTErr {
-	if err := usersdb.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := usersdb.Client.Prepare(queryGetUser)
+	defer stmt.Close()
+	if err != nil {
+		errors.NewInternalServerError(err.Error())
 	}
 
-	res := usersDB[u.ID]
-	if res == nil {
-		errors.NewNotFound(fmt.Sprintf("user not found: %d", u.ID))
+	res := stmt.QueryRow(u.ID)
+	if err := res.Scan(&u.ID, &u.FirstName, &u.LastName, &u.Email, &u.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errNoRows) {
+			return errors.NewNotFound(
+				fmt.Sprintf("user %d does not exist", u.ID),
+			)
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("unable to get UID %d: %s", u.ID, err.Error()),
+		)
 	}
-
-	u.ID = res.ID
-	u.FirstName = res.FirstName
-	u.LastName = res.LastName
-	u.Email = res.Email
-	u.DateCreated = res.DateCreated
-
 	return nil
 }
 
@@ -52,14 +54,14 @@ func (u *User) Save() *errors.RESTErr {
 			return errors.NewBadRequest(fmt.Sprintf("email address is already taken: %s", u.Email))
 		}
 		return errors.NewInternalServerError(
-			fmt.Sprintf("unable to save user: %s\n", err.Error()),
+			fmt.Sprintf("unable to save user: %s", err.Error()),
 		)
 	}
 
 	uID, err := insertResult.LastInsertId()
 	if err != nil {
 		return errors.NewInternalServerError(
-			fmt.Sprintf("unable to save user: %s\n", err.Error()),
+			fmt.Sprintf("unable to save user: %s", err.Error()),
 		)
 	}
 	u.ID = uint64(uID)
